d89620a21461726576f1b380dd66197cb941488a
Dawit Bireda <davebireda2394@gmail.com>
2024-03-22 10:16:20 +0300

    beverage

--- src/components/beverage.js
+++ src/components/beverage.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from 'react';
+import '../css/Beverage.css';
 import { Link } from "react-router-dom";
 
-const Beverage=()=>{
-    return(
-        <div>
-            <button className="button-33">
+const categories = [
+  {
+    name: 'Beverages',
+    items: [
+      { name: 'Soft drinks', price: '$2' },
+      { name: 'Iced tea', price: '$3' },
+      { name: 'Lemonade', price: '$3' },
+      { name: 'Coffee', price: '$4' },
+      { name: 'Mocktails', price: '$6' },
+      
+
+    ]
+  }
+];
+
+const Beverage = () => {
+  return (
+    <div className='beverage'>
+      {categories.map(category => (
+        <div key={category.name}>
+          <h2>{category.name}</h2>
+          <>
+            {category.items.map(item => (
+              <h4 key={item.name}>
+                {item.name} - {item.price}
+              </h4>
+            ))}
+          </>
+        </div>
+      ))}
+      <div>
+      <button className="button-33">
         <Link to="/">Go back to Home</Link>
         </button>
-            <h1>Beverage</h1>
-            
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Beverage
+export default Beverage;
